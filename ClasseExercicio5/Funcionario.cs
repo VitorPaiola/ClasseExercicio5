ef9e6d79b1d8604fc1c6a525d908d4c87e0323d7
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-23 14:12:42 -0300

    Adicionar arquivos de projeto.

---
+++ ClasseExercicio5/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace ClasseExercicio5 {
+    internal class Funcionario {
+
+        public string? Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            SalarioBruto += (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString() {
+            return Nome
+            + ", R$ "
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
